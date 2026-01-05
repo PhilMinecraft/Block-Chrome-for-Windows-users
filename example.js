62ff7b38cc2edfbb97f23f34525ef2ff2c9a81de
Arturo Rabino <arturo.rabino.3@outlook.com>
2026-01-05 18:58:54 +0800

    added more info

--- example.js
+++ example.js
@@ -7,7 +7,7 @@ const isWindows = ua.includes("Windows");
 const isChrome = uad.brands.some(b => b.brand === "Google Chrome");
 
 if (isWindows && isChrome) {
-    location.href = "microsoft-edge:" + window.location.origin; // redirect them to Edge or change the url to something.
-}
-
+    location.href = "microsoft-edge:" + window.location.origin; // redirect them to Edge or change the URL to something, but I'd suggest you to redirect them
+}                                                               // to same URL but in different browser (or Edge since it is pre-installed on most Windows),
+                                                                // that way, users won't hurt.
 // other browsers are not affected, only Chrome!
