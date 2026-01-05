61b805c580b2c939d86ff16b92d814b2dcf642b8
Arturo Rabino <arturo.rabino.3@outlook.com>
2026-01-05 18:49:01 +0800

    Add an example code

---
+++ example.js
@@ -0,0 +1,11 @@
+// Windows
+const ua = navigator.userAgent;
+const uad = navigator.userAgentData;
+const isWindows = ua.includes("Windows");
+
+// Chrome
+const isChrome = uad.brands.some(b => b.brand === "Google Chrome");
+
+if (isWindows && isChrome) {
+    location.href = "microsoft-edge:" + window.location.origin; // redirect them to Edge or change the url to something.
+}
